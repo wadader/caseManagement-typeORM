4ed17d962b49826318b7e25bb116a1c188e07877
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-04-16 17:46:16 +0700

    fix: fix abstract cases class

--- src/cases/cases.service.ts
+++ src/cases/cases.service.ts
@@ -18,20 +18,20 @@ export abstract class CasesService {
 }
 
 interface Cases {
-  findAll(): Promise<any>;
+  findAll(): Promise<any[]>;
   findOneById(id: number): Promise<any>;
   create(createCaseDto: CreateCaseDto): Promise<number>;
   resolve(id: number, resolution: CaseResultType): Promise<void>;
-  findAllJudgedBy(judgeId: number): Promise<any>;
-  findAllWithLawyer(lawyerId: number): Promise<any>;
+  findAllJudgedBy(judgeId: number): Promise<any[]>;
+  findAllWithLawyer(lawyerId: number): Promise<any[]>;
 }
 
 interface Judges {
   create({ name }: CreateJudgeDto): Promise<number>;
-  findAll(): Promise<any>;
+  findAll(): Promise<any[]>;
 }
 
 interface Lawyers {
   create(x: CreateLawyerDto): Promise<number>;
-  findAll(): Promise<any>;
+  findAll(): Promise<any[]>;
 }
